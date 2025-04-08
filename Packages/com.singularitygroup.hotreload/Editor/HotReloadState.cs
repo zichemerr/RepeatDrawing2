8023ab2b6abb30a7ceb4d0649b6f9d41e6639be6
zichemerr <zichemerr9@gmail.com>
2025-04-08 22:21:06 +0500

    Hot reload

---
+++ Packages/com.singularitygroup.hotreload/Editor/HotReloadState.cs
@@ -0,0 +1,61 @@
+using UnityEditor;
+
+namespace SingularityGroup.HotReload.Editor {
+    internal static class HotReloadState {
+        private const string ServerPortKey = "HotReloadWindow.ServerPort";
+        private const string LastPatchIdKey = "HotReloadWindow.LastPatchId";
+        private const string ShowingRedDotKey = "HotReloadWindow.ShowingRedDot";
+        private const string ShowedEditorsWithoutHRKey = "HotReloadWindow.ShowedEditorWithoutHR";
+        private const string ShowedFieldInitializerWithSideEffectsKey = "HotReloadWindow.ShowedFieldInitializerWithSideEffects";
+        private const string ShowedFieldInitializerExistingInstancesEditedKey = "HotReloadWindow.ShowedFieldInitializerExistingInstancesEdited";
+        private const string ShowedFieldInitializerExistingInstancesUneditedKey = "HotReloadWindow.ShowedFieldInitializerExistingInstancesUnedited";
+        private const string RecompiledUnsupportedChangesOnExitPlaymodeKey = "HotReloadWindow.RecompiledUnsupportedChangesOnExitPlaymode";
+        private const string RecompiledUnsupportedChangesInPlaymodeKey = "HotReloadWindow.RecompiledUnsupportedChangesInPlaymode";
+
+        public static int ServerPort {
+            get { return SessionState.GetInt(ServerPortKey, RequestHelper.defaultPort); }
+            set { SessionState.SetInt(ServerPortKey, value); }
+        }
+        
+        public static string LastPatchId {
+            get { return SessionState.GetString(LastPatchIdKey, string.Empty); }
+            set { SessionState.SetString(LastPatchIdKey, value); }
+        }
+        
+        public static bool ShowingRedDot {
+            get { return SessionState.GetBool(ShowingRedDotKey, false); }
+            set { SessionState.SetBool(ShowingRedDotKey, value); }
+        }
+        
+        public static bool ShowedEditorsWithoutHR {
+            get { return SessionState.GetBool(ShowedEditorsWithoutHRKey, false); }
+            set { SessionState.SetBool(ShowedEditorsWithoutHRKey, value); }
+        }
+        
+        public static bool ShowedFieldInitializerWithSideEffects {
+            get { return SessionState.GetBool(ShowedFieldInitializerWithSideEffectsKey, false); }
+            set { SessionState.SetBool(ShowedFieldInitializerWithSideEffectsKey, value); }
+        }
+        
+        public static bool ShowedFieldInitializerExistingInstancesEdited {
+            get { return SessionState.GetBool(ShowedFieldInitializerExistingInstancesEditedKey, false); }
+            set { SessionState.SetBool(ShowedFieldInitializerExistingInstancesEditedKey, value); }
+        }
+        
+        public static bool ShowedFieldInitializerExistingInstancesUnedited {
+            get { return SessionState.GetBool(ShowedFieldInitializerExistingInstancesUneditedKey, false); }
+            set { SessionState.SetBool(ShowedFieldInitializerExistingInstancesUneditedKey, value); }
+        }
+        
+        public static bool RecompiledUnsupportedChangesOnExitPlaymode {
+            get { return SessionState.GetBool(RecompiledUnsupportedChangesOnExitPlaymodeKey, false); }
+            set { SessionState.SetBool(RecompiledUnsupportedChangesOnExitPlaymodeKey, value); }
+        }
+        
+        public static bool RecompiledUnsupportedChangesInPlaymode {
+            get { return SessionState.GetBool(RecompiledUnsupportedChangesInPlaymodeKey, false); }
+            set { SessionState.SetBool(RecompiledUnsupportedChangesInPlaymodeKey, value); }
+        }
+    }
+
+}
